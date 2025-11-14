6e8bf55a04070cff1b07b5a50b30f39286e969f0
Yns-data <118600486+Yns-data@users.noreply.github.com>
2025-11-15 00:34:24 +0100

    Simplify setup.py by removing package_dir
    Removed package directory specification to use default.

--- MongoDb/mongo_db_interaction/setup.py
+++ MongoDb/mongo_db_interaction/setup.py
@@ -1,8 +1,7 @@
 from setuptools import setup, find_packages
 
 setup(
-    name="mongo_db_interaction",
+    name="mongo_db_interaction", 
     version="0.1",
-    packages=find_packages(where="MongoDb"),
-    package_dir={"": "MongoDb"},
+    packages=find_packages(),  
 )
