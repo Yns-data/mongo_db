607981196399967bb2958ec8b22360e64fc823dc
yns-data <younesessoualhi@gmail.com>
2025-11-17 14:59:20 +0000

    updating the linking script and adding sql requists file

--- Workflow_Mongodb_Postgrsql/mongodb_pstgresdb_link.py
+++ Workflow_Mongodb_Postgrsql/mongodb_pstgresdb_link.py
@@ -7,6 +7,7 @@ import gzip
 import os
 from dotenv import load_dotenv
 from google.cloud import storage
+from sql_requests import SQL_QUERIES
 
 # Load environment variables
 print("Loading environment variables...")
@@ -25,8 +26,8 @@ def get_dataframe_from_mongodb(date:str):
         print(f"DataFrame loaded with {len(df)} rows and {len(df.columns)} columns.")
         return df
     except Exception as e:
-        print(e)
-        df = pd.read_csv("/Workflow_Mongodb_Postgrsql/")
+        raise Exception("Error occurred while loading data from mongodb")
+        
 
 
 # PostgreSQL configuration
@@ -42,21 +43,17 @@ def load_postgres_config():
     print(f"PostgreSQL config loaded. Target table: '{table_name}'")
     return table_name, postgre_db_config
 # PostgreSQL insertion
-def copy_dataframe_to_postgres(df, table_name, postgre_db_config):
+def copy_dataframe_to_postgres(df:pd.DataFrame, table_name:str, postgre_db_config:dict):
     """
     Insert a pandas DataFrame into a PostgreSQL table using COPY and context managers.
     """
-
-    # Préparation tampon CSV pour COPY
     print("Preparing data for COPY...")
     buffer = StringIO()
-    df.to_csv(buffer, index=False, header=False)  # COPY ne veut pas d'en-têtes
+    df.to_csv(buffer, index=False, header=False)
     buffer.seek(0)
-
     columns = ', '.join(df.columns)
     sql = f"COPY {table_name} ({columns}) FROM STDIN WITH CSV"
 
-    # Connexion avec context manager
     print("Connecting to PostgreSQL...")
     with psycopg2.connect(**postgre_db_config) as conn:
         with conn.cursor() as cur:
@@ -65,34 +62,70 @@ def copy_dataframe_to_postgres(df, table_name, postgre_db_config):
 
             cur.copy_expert(sql=sql, file=buffer)
 
-        # Le commit et le close sont gérés automatiquement par psycopg2
-
     print("Data inserted successfully.")
+
+def run_sql_from_string(sql_string:str, postgre_db_config:dict):
+    """
+    Execute multiple SQL statements from a string using psycopg2
+    with automatic connection and cursor management.
+    """
+
+    try:
+        with psycopg2.connect(
+            **postgre_db_config
+        ) as conn:
+
+            with conn.cursor() as cur:
+
+                print("[INFO] Running SQL string...")
+                
+                statements = sql_string.split(";")
+
+                for stmt in statements:
+                    stmt = stmt.strip()
+                    if stmt:
+                        try:
+                            print(f"[INFO] Executing: {stmt[:80]}...")
+                            cur.execute(stmt)
+                        except Exception as e:
+                            print(f"[ERROR] SQL execution failed: {e}")
+
+    finally:
+        print("[INFO] PostgreSQL operations finished.")
+
 
 if __name__=="__main__":
     # loading inputs 
-    # df = get_dataframe_from_mongodb(date="20251114-15-30-45")
-    df = pd.read_csv("Workflow_Mongodb_Postgrsql/afklm_removed_sch_flight_from_mongo_filtered_20251117-12-15-18.csv.gz")
     
     table_name, postgre_db_config = load_postgres_config()
-    # Google Cloud Storage configuration
+    
     BUCKET_NAME = os.getenv("BUCKET_NAME")
     SOURCE_BLOB_NAME = os.getenv("MAPPING_FILE_BLOB_NAME")
     print(f"Fetching column mapping JSON from GCS bucket '{BUCKET_NAME}', blob '{SOURCE_BLOB_NAME}' ...")
-
-    # Load JSON column mapping from GCS
+    
     storage_client = storage.Client()
     bucket = storage_client.bucket(BUCKET_NAME)
     blob = bucket.blob(SOURCE_BLOB_NAME)
+    
 
     json_data = blob.download_as_bytes()
     column_mapping = json.load(BytesIO(json_data))
     print(f"Column mapping loaded with {len(column_mapping)} entries.")
 
+    df = get_dataframe_from_mongodb(date="20251114-15-30-45")
+    # df = pd.read_csv("Workflow_Mongodb_Postgrsql/afklm_removed_sch_flight_from_mongo_filtered_20251117-12-15-18.csv.gz")
+
     # Rename DataFrame columns according to the mapping
     print("Renaming DataFrame columns...")
     df.rename(columns=column_mapping, inplace=True)
     print("Columns renamed successfully.")
-
     # insertion of data
-    copy_dataframe_to_postgres(df, table_name, postgre_db_config)
+    try:
+        copy_dataframe_to_postgres(df, table_name, postgre_db_config)
+    except Exception as e:
+        raise Exception("An Error has occured while copying mongodb data into postgre Sql database")
+    
+    # executing transformation requests
+    run_sql_from_string(SQL_QUERIES, postgre_db_config)
+
+
